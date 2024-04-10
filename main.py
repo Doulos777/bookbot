466f6f6b526275ef32a5075374254f9ccfa93102
Doulos777 <AWRoberson77@gmail.com>
2024-04-10 12:47:12 -0400

    saving changes

---
+++ main.py
@@ -0,0 +1,21 @@
+def main():
+    # Define the path to your file
+    path_to_file = 'books/frankenstein.txt'
+    
+    # Open the file and read its contents
+    with open(path_to_file, 'r') as file:
+        file_contents = file.read()
+        
+        # Print the contents to the console
+        print(file_contents)
+
+        # Counting words
+        print(len(file_contents.split()))
+
+        # Counting Letters
+        print(len(file_contents.split()))
+
+
+# Calling the main function
+if __name__ == '__main__':
+    main()
